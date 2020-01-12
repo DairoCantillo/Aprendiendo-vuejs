1ef7ba43d3f7a30a6126bf7d4d76d7b2b736f5ef
Dairo Cantillo <cantillodairo62@gmail.com>
2020-01-11 20:16:54 -0500

    uso de v-if, v-for, v-model y on::click

--- Vue-CDN/script.js
+++ Vue-CDN/script.js
@@ -6,7 +6,26 @@ const app = new Vue({
             {nombre: 'Harry Potter', cantidad: 4},
             {nombre: 'Narnia', cantidad: 0},
             {nombre: 'Soy el numero 4', cantidad: 4},
-        ]
+        ],
+        nuevoLibro: '',
+        total:  0,
+    },
+    methods:{
+        agregarLibro(){
+            this.libros.push({
+                nombre: this.nuevoLibro, cantidad: 0
+            })
+            this.nuevoLibro= ""
+        },
+    },
+    computed: {
+        sumarLibros(){
+            this.total = 0;
+            for(libro of this.libros){
+                this.total = this.total + libro.cantidad;
+            }
+            return this.total;
+        }
     }
 
 })
