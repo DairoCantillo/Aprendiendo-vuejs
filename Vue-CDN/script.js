6c1daacb51d26e47eb7aaf3f5a59ce585e08e9f3
Dairo Cantillo <cantillodairo62@gmail.com>
2020-01-11 19:16:59 -0500

    hola mundo en vue js

---
+++ Vue-CDN/script.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Fantasia',
+        libros: [
+            {nombre: 'Harry Potter', cantidad: 4},
+            {nombre: 'Narnia', cantidad: 0},
+            {nombre: 'Soy el numero 4', cantidad: 4},
+        ]
+    }
+
+})
