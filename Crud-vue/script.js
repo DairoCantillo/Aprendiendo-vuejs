38b0e0e2c21ac5bdb06d863234149b81bf01f3ed
Dairo Cantillo <cantillodairo62@gmail.com>
2020-01-15 23:01:53 -0500

    Crud con el local storage

--- Crud-vue/script.js
+++ Crud-vue/script.js
@@ -1,13 +1,36 @@
 const app = new Vue({
     el: '#app',
     data:{
+        titulo:'App De GYM Con VueJs',
+        tareas:[],
+        nuevaTarea:'',
 
     },
     methods:{
-
+        agregarTarea: function() {
+            this.tareas.push({
+                nombre: this.nuevaTarea,
+                estado: false
+            });
+            this.nuevaTarea='';
+            localStorage.setItem('gym-vue', JSON.stringify(this.tareas))
+        },
+        editarTarea:function(index) {
+            this.tareas[index].estado = true;
+            localStorage.setItem('gym-vue', JSON.stringify(this.tareas))
+        },
+        eliminar:function(index) {
+            this.tareas.splice(index,1);
+            localStorage.setItem('gym-vue', JSON.stringify(this.tareas))
+        },
     },
-    computed: {
-
+    created: function() {
+        let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
+        if (datosDB == null) {
+            this.tareas = [];
+        }else{
+            this.tareas = datosDB;
+        }
     }
 
-})
+});
